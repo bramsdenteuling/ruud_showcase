26972e3632b6b468c11efe2780002b4f15eb1b73
Bram den Teuling <bramdenteuling@gmail.com>
2011-04-15 18:07:23 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RuudShowcase::Application.config.secret_token = 'd0113e5c3fe1909b8b9066fbd599eb4372558c859b91456e179b690c336b815e8f11362c0c2db20ec2dd1a78c4ec5b2b9a16df88be9b337130f75f8bf425c4d0'
