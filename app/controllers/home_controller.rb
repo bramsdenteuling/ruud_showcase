32e6a8e92cab4a6025b43b0744b720862c2dcb4c
Andres Lamont <alamont@gmail.com>
2011-05-03 02:52:10 +0200

    Render imageloader instead of home. Change redirect to render

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,17 @@
 class HomeController < ApplicationController
+
   def index
+	#render :template => 'imageloader/index'
   end
   
   def contact
       @user = User.new
   end
 
-  def send_mail
-      ContactMailer.contact_email(params[:email]).deliver
-      redirect_to("", :notice => 'Your message has been send!')
-  end
+  #def send_mail
+  #    ContactMailer.contact_email(params[:email]).deliver
+      #redirect_to("", :notice => 'Your message has been send!')
+  #    render :notice => 'Your message has been send!'
+  #end
 
 end
