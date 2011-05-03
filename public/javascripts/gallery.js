0a8030394ec0afd1c5251bb1d16f31ad56302657
Andres Lamont <alamont@gmail.com>
2011-05-03 03:14:19 +0200

    Added slide transition. Added pan for cropped pictures.

--- public/javascripts/gallery.js
+++ public/javascripts/gallery.js
@@ -7,7 +7,10 @@ var flickr = new Galleria.Flickr('3ec6a244b24cea97d27b1a0c9c8cd143');
 $('#gallery').galleria({
     //height: 600,
 	//#width: 960,
-	imageCrop: true
+	transitionInitial: 'fade',
+	transition: 'slide',
+	imageCrop: true,
+	imagePan: true
 });
 //alert(data);
 //flickr.search('sweden', function(data) { 
