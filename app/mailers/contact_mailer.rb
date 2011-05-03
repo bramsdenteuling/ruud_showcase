b43e64c994090a30a18e96a0615a7ccedfd16a1f
Andres Lamont <alamont@gmail.com>
2011-05-03 02:53:44 +0200

    Removed parameters to initialization

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,17 +1,26 @@
 class ContactMailer < ActionMailer::Base
-  default :from => "from@example.com"
+  #default :from => "from@example.com"
   
-  def contact_email(email_params)
-      # You only need to customize @recipients.
-      @recipients = "bramdenteuling@gmail.com"
-      @from = email_params[:name] + " <" + email_params[:address] + ">"
-      @subject = email_params[:subject]
-      @sent_on = Time.now
-      @body["email_body"] = email_params[:body]
-      @body["email_name"] = email_params[:name]
-      content_type "text/html"
-      mail(:to => @recipients, :subject => @subject)
+  #def contact_email(email_params)
+      ## You only need to customize @recipients.
+      #@recipients = "bramdenteuling@gmail.com"
+      #@from = email_params[:name] + " <" + email_params[:address] + ">"
+      #@subject = email_params[:subject]
+      #@sent_on = Time.now
+      #@body["email_body"] = email_params[:body]
+      #@body["email_name"] = email_params[:name]
+      #content_type "text/html"
+      #mail(:to => @recipients, :subject => @subject)
+  #end
+  
+  def contact_me(contact_message)
+    @contact_message = contact_message
+
+    mail(:to => App.email.address,
+         :from => contact_message.email,
+         :subject => contact_message.subject)
   end
+
   
   
 end
