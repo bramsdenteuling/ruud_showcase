83a8226b8fbdbe990d0f6203770079d169e8728b
Andres Lamont <alamont@gmail.com>
2011-05-03 03:05:24 +0200

    Moved galleria javascript from apllication view to this file

---
+++ public/javascripts/gallery.js
@@ -0,0 +1,25 @@
+Galleria.loadTheme('/javascripts/galleria/themes/fullscreen/galleria.fullscreen.js');
+
+
+var flickr = new Galleria.Flickr('3ec6a244b24cea97d27b1a0c9c8cd143');
+
+//flickr.getSet('
+$('#gallery').galleria({
+    //height: 600,
+	//#width: 960,
+	imageCrop: true
+});
+//alert(data);
+//flickr.search('sweden', function(data) { 
+	//$('#gallery').galleria({ 
+		//dataSource: data,
+		//height: 600,
+		//width: 960,
+		//imageCrop: true
+	//}); 
+//});
+//$('#gallery').galleria({
+	//height: 600,
+	//width: 960,
+	//imageCrop: true
+//});
