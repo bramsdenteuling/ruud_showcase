29039643c520c08d4e2a3285bd1dd7df573e3aa5
Andres Lamont <alamont@gmail.com>
2011-05-03 02:56:28 +0200

    Add Flickr API key and secret (ow noes!!) key.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 RuudShowcase::Application.initialize!
+FlickRaw.api_key = '7478850d12326df4a10e2da661ae61e7'
+FlickRaw.shared_secret = '6506d4d206cfe397'
