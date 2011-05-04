e8cccb6530edde57934a8ec5792672e689a078f2
Andres Lamont <alamont@gmail.com>
2011-05-04 15:50:30 +0200

    Converted queries to mongoid

--- app/controllers/infobox_controller.rb
+++ app/controllers/infobox_controller.rb
@@ -2,32 +2,36 @@ class InfoboxController < ApplicationController
 	before_filter :authenticate_user!, :except => [:index]  
 
 	def index
-		@info = Info.find_by_id('info')
+		@info = Info.where(infoid: 'info').first
 		render :layout => 'clean'
 	end
 	
 	def edit
-		if Info.find_by_id('info') == nil
-				info = Info.new(:'id' => 'info', :title => 'Info',:text => 'TEXTEXTEXT');
+		if Info.where(infoid: 'info').first == nil
+				info = Info.new(infoid: 'info', title: 'Info',text: 'TEXTEXTEXT')
 				info.save
 		end
-		@info = Info.find_by_id('info')
+		@info = Info.where(infoid: 'info').first
+		
 		render :layout => 'clean'
 	end
 	
 	def update
-		if Info.find_by_id('info') == nil
-			info = Info.new(:'id' => 'info', :title => 'Info',:text => 'TEXTEXTEXT');
+		if Info.where(infoid: 'info').first == nil
+			info = Info.new(infoid: 'info', title: 'Info',text: 'TEXTEXTEXT')
 			info.save
 		else
 			title = params[:info][:title]
 			text = params[:info][:text]
-			RAILS_DEFAULT_LOGGER.debug title
-			info = Info.first
-			info.update_attributes('title' => title, 'text' => text)
+			info = Info.where(infoid: 'info').first
+			info.write_attributes(title: title, text: text)
 			info.save
 		end
-		render :layout => 'clean'
+		
+		respond_to do |format|
+			format.js {render :layout => 'clean'}
+		end
+		
 	end
 	
 end
