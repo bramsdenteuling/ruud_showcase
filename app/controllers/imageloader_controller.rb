441839840000b5a91621b90b5cb8af113884db0b
Andres Lamont <alamont@gmail.com>
2011-05-04 17:15:20 +0200

    Removed unused code

--- app/controllers/imageloader_controller.rb
+++ app/controllers/imageloader_controller.rb
@@ -5,9 +5,6 @@ class ImageloaderController < ApplicationController
 		@photos = flickr.photosets.getPhotos(:photoset_id => '72157625465226438')
 		@photos = @photos.to_hash()
 		@photos = @photos["photo"]
-		
-		#@photosize = flickr.photos.getSizes(:photo_id => '5206577114')
-		#RAILS_DEFAULT_LOGGER.debug @photosize
 	end
 	
 end
