7f416200b5ece0ff0bfda3c8a91aff1b5cd43cf6
Andres Lamont <alamont@gmail.com>
2011-05-07 01:01:31 +0200

    Use originalsecret insted of secret when constructing url for original image.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,13 @@
 module ApplicationHelper
 
-	def make_flickr_url(r, size = 'o')
-		"http://farm#{r.farm}.static.flickr.com/" <<
+	def make_flickr_url(r, size)
+		if size == 'o'
+			"http://farm#{r.farm}.static.flickr.com/" <<
+			"#{r.server}/#{r.id}_#{r.originalsecret}_#{size}.jpg"
+		else
+			"http://farm#{r.farm}.static.flickr.com/" <<
 			"#{r.server}/#{r.id}_#{r.secret}_#{size}.jpg"
+		end
 	end
 
 end
