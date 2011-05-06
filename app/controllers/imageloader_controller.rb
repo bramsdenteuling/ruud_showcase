39999cb8e9c42aca94d2e308c2425ae26df9456d
Andres Lamont <alamont@gmail.com>
2011-05-07 01:00:47 +0200

    Dramatically improve Flickr performance for original images by querying the original url in one query. Loading ofcourse is still very slow with 5+ MB photos.

--- app/controllers/imageloader_controller.rb
+++ app/controllers/imageloader_controller.rb
@@ -2,7 +2,7 @@ class ImageloaderController < ApplicationController
 
 	def index
 		#@photos = flickr.photos.search(:per_page => 20, :extras => 'original_format, o_dims', :user_id => '56210880@N07')
-		@photos = flickr.photosets.getPhotos(:photoset_id => '72157625465226438')
+		@photos = flickr.photosets.getPhotos(:photoset_id => '72157625465226438', :extras => 'original_format')
 		@photos = @photos.to_hash()
 		@photos = @photos["photo"]
 	end
