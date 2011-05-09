129f82566ed1c01b025e3cc983d643f8c0b7dcd9
Andres Lamont <alamont@gmail.com>
2011-05-09 16:26:24 +0200

    Catch error when photoset_id does not exist. Show epic fail.

--- app/controllers/imageloader_controller.rb
+++ app/controllers/imageloader_controller.rb
@@ -1,10 +1,21 @@
 class ImageloaderController < ApplicationController
+  
+  rescue_from FlickRaw::FailedResponse, :with => :failed_response
 
 	def index
 		#@photos = flickr.photos.search(:per_page => 20, :extras => 'original_format, o_dims', :user_id => '56210880@N07')
-		@photos = flickr.photosets.getPhotos(:photoset_id => '72157625465226438', :extras => 'original_format')
+    @set = Photoset.where(doc_id: '1').first;
+    set_id = @set["set_id"]
+    if @photos = flickr.photosets.getPhotos(:photoset_id => set_id, :extras => 'original_format')
+    end
 		@photos = @photos.to_hash()
 		@photos = @photos["photo"]
 	end
+  
+  def failed_response
+    @photos = flickr.photos.search(:per_page => 10, :text => 'epic fail', :sort => 'relevance')
+    flash[:notice] = "Unkown Flick set ID. Showing epic fail photos"
+    render 'index'
+  end
 	
 end
